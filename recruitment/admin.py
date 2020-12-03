d01ff6e2f035ce35612e41fe601b157b0f5b1916
AlexanderLyaksa <sanchos2004@gmail.com>
2020-12-03 15:12:55 +0300

    Added skill and social field to list_display

--- recruitment/admin.py
+++ recruitment/admin.py
@@ -11,10 +11,22 @@ class RatingInline(admin.TabularInline):
 class CandidateAdmin(admin.ModelAdmin):
     """Кастомизация админки."""
 
-    list_display = ['title', 'salary']
+    list_display = ['title', 'get_skill', 'get_social', 'salary']
     search_fields = ['source_id', 'title']
     inlines = [RatingInline]
     list_per_page = 40
+
+    def get_skill(self, obj):
+        """Вывод поля skill модели Rating"""
+        return obj.ratings.skill
+
+    get_skill.short_description = 'Skill'
+
+    def get_social(self, obj):
+        """Вывод поля social модели Rating"""
+        return obj.ratings.social
+
+    get_skill.short_description = 'Social'
 
 
 admin.site.register(Level)
